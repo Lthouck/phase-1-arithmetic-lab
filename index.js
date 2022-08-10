d5b33989145d32530e8b9971d6ff5f70a495dcfe
Lesslie Houck <Lesslie00@outlook.com>
2022-08-10 13:20:30 -0700

    commit tree 50887309dac91092b90fecd1a5b62d909afa3d74

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 5)
+
+const num3 = 9
+const num4 = 5
+const mod = num3 % num4
+
+const max = Math.max(1, 9, 11, 15, 20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let multiply = 62
+// let num1 = 31
+// let num2 = 2
+// function multiply(num1, num2) {
+//     return num1 *= num2
+// }
+
+// const random = Math.floor(Math.random() * 10) + 1 
+// console.log(random)
+
+// let mod = 4
+// let num3 = 20
+// let num4 = 5
+// function mod(num3, num4) {
+//     return num3 / num4
+// }
+
+//  const max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+//  console.log(max)
